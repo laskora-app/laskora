3b9caf1ae5e990adb301d4e1e78cf57a73a430b3
ahmed al-sultan <alsultanahmed034@gmail.com>
2026-03-12 20:58:47 +0200

    fix resend smtp tls

--- config/settings.py
+++ config/settings.py
@@ -127,7 +127,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.resend.com"
 EMAIL_PORT = 587
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = "resend"
 EMAIL_HOST_PASSWORD = os.environ.get("RESEND_API_KEY")
