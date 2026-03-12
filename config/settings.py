911f8fc9ac1274dcd0228388d0a95f74b7c184f6
ahmed al-sultan <alsultanahmed034@gmail.com>
2026-03-12 22:03:42 +0200

    remove smtp settings and use resend api

--- config/settings.py
+++ config/settings.py
@@ -123,13 +123,6 @@ STATICFILES_DIRS = [
 ]
 import os
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-
-EMAIL_HOST = "smtp.resend.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = "resend"
 RESEND_API_KEY = os.environ.get("RESEND_API_KEY")
 
 DEFAULT_FROM_EMAIL = "onboarding@resend.dev"
